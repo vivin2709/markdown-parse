4758a40a4bfa8953e31e8adfeda681f9f922e99e
vivin2709 <vivinvinil05@gmail.com>
2022-01-26 19:55:54 -0800

    created tester file
    javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
    
    java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest

---
+++ MarkdownTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+//static
+import org.junit.*;
+
+public class MarkdownParseTest {
+    //declare a test method
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+}

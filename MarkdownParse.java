5e20474e983effcd5bee06239828a46054dab36f
vivin2709 <vivinvinil05@gmail.com>
2022-01-28 12:47:47 -0800

    symptom rechecking

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -15,15 +15,17 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            
             if(openParen<0 || closeParen<0) {
-                break;
+                continue;
             }
-            /*  if(nextOpenBracket!=0 && markdown.charAt(nextOpenBracket-1)=='!')
+              /* if(nextOpenBracket!=0 && markdown.charAt(nextOpenBracket-1)=='!')
             {
                 currentIndex = closeParen + 1;
                 continue;
             }
             */
+            
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
